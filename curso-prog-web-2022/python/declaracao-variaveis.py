77eadfc73b9831983ecf439c052d90b453ef1492
Marlison Soares da Silva <marlison.silva@academico.ifrn.edu.br>
2022-07-18 11:31:16 -0300

    python - var, cond, estrutura

---
+++ curso-prog-web-2022/python/declaracao-variaveis.py
@@ -0,0 +1,11 @@
+# Inteiros
+a = 28
+# Ponto flutuante
+pi = 3.1415
+# String
+name = 'Romerito Campos'
+# Boolean
+b = True
+# Valor vazio ou dado do tipo nulo
+x = None
+# Variável declarada de forma errada

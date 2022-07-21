ee2a0661c2ed879dc6f92887cb7fe989ecd7bed2
Marlison Soares da Silva <marlison.silva@academico.ifrn.edu.br>
2022-07-21 11:51:06 -0300

    models

--- prog-internet/django/proteste-aqui/projeto/protesteaqui/models.py
+++ prog-internet/django/proteste-aqui/projeto/protesteaqui/models.py
@@ -1,3 +1,11 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    class Meta:
+        verbose_name_plural = "professores"
+    nome = models.CharField(max_length=124, default='')
+    disciplina = models.CharField(max_length=124, default='')
+    cpf = models.CharField(max_length=14, default='')
+    idade = models.IntegerField(default=0)

66d735e7b0f9fa393b35e24d86bf18e1f617c3e7
Marlison Soares da Silva <marlison.silva@academico.ifrn.edu.br>
2022-07-27 11:50:09 -0300

    sla

--- prog-internet/django/proteste-aqui/projeto/protesteaqui/admin.py
+++ prog-internet/django/proteste-aqui/projeto/protesteaqui/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from protesteaqui.models import Professor
+from protesteaqui.models import Professor, Avaliacao
 
 # Register your models here.
 admin.site.register(Professor)
+admin.site.register(Avaliacao)
--- prog-internet/django/proteste-aqui/projeto/protesteaqui/models.py
+++ prog-internet/django/proteste-aqui/projeto/protesteaqui/models.py
@@ -1,6 +1,7 @@
 from tabnanny import verbose
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
 # Create your models here.
 class Professor(models.Model):
     class Meta:
@@ -8,4 +9,19 @@ class Professor(models.Model):
     nome = models.CharField(max_length=124, default='')
     disciplina = models.CharField(max_length=124, default='')
     cpf = models.CharField(max_length=14, default='')
-    idade = models.IntegerField(default=0)
+    idade = models.IntegerField(
+        validators=[MinValueValidator(0)]
+    )
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+class Avaliacao(models.Model):
+    class Meta:
+        verbose_name_plural = "avaliacoes"
+    descricao = models.TextField(max_length=500)        
+    nota = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)]
+    )
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)

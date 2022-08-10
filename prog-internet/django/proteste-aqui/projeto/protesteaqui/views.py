b6e4cff59cc1de10d4103eeaf82f70cfd3a6803f
Marlison Soares da Silva <marlison.silva@academico.ifrn.edu.br>
2022-08-10 11:56:03 -0300

    models na view

--- prog-internet/django/proteste-aqui/projeto/protesteaqui/views.py
+++ prog-internet/django/proteste-aqui/projeto/protesteaqui/views.py
@@ -3,6 +3,9 @@ from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+
+from protesteaqui.models import Professor
+
 # Create your views here.
 
 def index(request):
@@ -11,3 +14,8 @@ def index(request):
 def home(request):
     site = "<html><body> <h1>HOME</h1> </body></html>" , datetime.now().strftime('%H:%M:%S')
     return HttpResponse(site)
+    
+def teste(request):
+    prof = Professor.objects.get(pk=1)
+    parametros = {"jogador": prof.nome, "disciplina": prof.disciplina, "idade": prof.idade}
+    return render(request, 'teste.html', parametros)
